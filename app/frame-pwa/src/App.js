2fc5cf61bcacda1c450b2c51bb5926b78fa5b0ff
Ayush Daga <dagaayush1205@gmail.com>
2025-02-13 16:29:07 +0530

    included the camera feed in frontend

--- app/frame-pwa/src/App.js
+++ app/frame-pwa/src/App.js
@@ -1,14 +1,13 @@
-
 import React from "react";
+import "./App.css"; // Import the CSS file
 
 function App() {
   return (
     <div className="app-container">
-      <h1>Processed Video Stream</h1>
-      <img src="http://localhost:5000/video_feed" alt="Processed Frames" />
+      <h1>Face & Hand Tracking</h1>
+      <img src="http://localhost:5000/video_feed" alt="Video Stream" className="video-feed" />
     </div>
   );
 }
 
 export default App;
-
