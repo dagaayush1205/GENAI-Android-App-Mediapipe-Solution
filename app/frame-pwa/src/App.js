04cfc19e93317d0bc3ccd846ae7c2aac5d85a481
Ayush Daga <dagaayush1205@gmail.com>
2025-02-18 12:00:00 +0530

    included video port from flask

--- app/frame-pwa/src/App.js
+++ app/frame-pwa/src/App.js
@@ -1,13 +1,75 @@
-import React from "react";
-import "./App.css"; // Import the CSS file
+
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [selectedOptions, setSelectedOptions] = useState({
+    faceTracking: true,
+    handTracking: true,
+  });
+
+  // Function to send data to Flask
+  const sendCheckboxData = (updatedOptions) => {
+    fetch("http://localhost:5000/update_options", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedOptions),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log("Response from Flask:", data))
+      .catch((error) => console.error("Error:", error));
+  };
+
+  // Handle checkbox change
+  const handleCheckboxChange = (event) => {
+    const updatedOptions = {
+      ...selectedOptions,
+      [event.target.name]: event.target.checked,
+    };
+
+    setSelectedOptions(updatedOptions); // Update UI
+    sendCheckboxData(updatedOptions); // Send to Flask
+  };
+
   return (
     <div className="app-container">
       <h1>Face & Hand Tracking</h1>
-      <img src="http://localhost:5000/video_feed" alt="Video Stream" className="video-feed" />
+
+      {/* Checkboxes */}
+      <div className="checkbox-container">
+        <label>
+          <input
+            type="checkbox"
+            name="faceTracking"
+            checked={selectedOptions.faceTracking}
+            onChange={handleCheckboxChange}
+          />
+          Enable Face Tracking
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            name="handTracking"
+            checked={selectedOptions.handTracking}
+            onChange={handleCheckboxChange}
+          />
+          Enable Hand Tracking
+        </label>
+      </div>
+
+      {/* Video Feed */}
+      <img
+        src="http://localhost:5000/video_feed"
+        alt="Video Stream"
+        className="video-feed"
+      />
     </div>
   );
 }
+
+
 
 export default App;
