fecd590c7c8ce5af0e10b2480a8f655fb76ed9ca
Ayush Daga <ad6027@srmist.edu.in>
2025-03-11 15:29:31 +0530

    integerating intel arc gpu

--- attone/phi.py
+++ attone/phi.py
@@ -1,13 +1,15 @@
 import torch
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
-
-
-model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1_5", torch_dtype="auto")
-tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5")
+from transformers import pipelinelr
+AutoTokenizer
+from intel_extension_for_transformers.transformers import AutoModelForCausalLM
+model_name="microsoft/phi-1.5"
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 print("Enter input")
 text = input()
 inputs = tokenizer(text, return_tensors="pt")
 output = model.generate(**inputs, max_length=100)
 print("OUTPUT")
 print(tokenizer.decode(output[0]))
+print(output)
 
